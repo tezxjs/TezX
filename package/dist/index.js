e12412f4fb1c5926e421b82a5d12b7f07fa8a39b
SRAKIB17 <rakibulssc5@gmail.com>
2025-04-03 19:32:10 +0600

    republish

--- package/dist/index.js
+++ package/dist/index.js
@@ -2,4 +2,4 @@ export * from "./adapter";
 export { Router } from "./router";
 export { TezX } from "./server";
 export { useParams } from "./utils/params";
-export let version = "1.0.14";
+export let version = "1.0.19";
