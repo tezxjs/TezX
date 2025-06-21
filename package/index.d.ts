84b84c7b95514834a61bd15d37165c7dbdd4906b
SRAKIB17 <rakibulssc5@gmail.com>
2025-06-22 01:56:28 +0600

    root-default-ecport

--- package/index.d.ts
+++ package/index.d.ts
@@ -1,3 +1,6 @@
+import { Router } from "express";
+import { TezX } from "./core/server.js";
+import { useParams } from "./utils/params.js";
 export { Router } from "./core/router.js";
 export type { Callback, ctx as Context, Middleware, NextCallback, RouterConfig, StaticServeOption } from "./core/router.js";
 export type { AdapterType } from "./core/config.js";
@@ -5,6 +8,13 @@ export type { CookieOptions, ResponseHeaders } from "./core/context.js";
 export type { NetAddr as AddressType, ConnAddress, FormDataOptions, HTTPMethod } from "./core/request.js";
 export { TezX } from "./core/server.js";
 export type { TezXConfig, TezXServeOptions } from "./core/server.js";
-export { useParams } from "./utils/params.js";
 export type { UrlRef } from "./utils/url.js";
+export { useParams };
 export declare let version: string;
+declare const _default: {
+    Router: typeof Router;
+    TezX: typeof TezX;
+    useParams: typeof useParams;
+    version: string;
+};
+export default _default;
--- package/index.js
+++ package/index.js
@@ -1,4 +1,13 @@
+import { Router } from "express";
+import { TezX } from "./core/server.js";
+import { useParams } from "./utils/params.js";
 export { Router } from "./core/router.js";
 export { TezX } from "./core/server.js";
-export { useParams } from "./utils/params.js";
-export let version = "2.0.4";
+export { useParams };
+export let version = "2.0.5";
+export default {
+    Router,
+    TezX,
+    useParams,
+    version,
+};
