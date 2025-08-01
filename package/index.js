6a007cc57b67a0ef2cf2c31dbffdc46c257a81c5
SRAKIB17 <rakibulssc5@gmail.com>
2025-08-02 05:40:23 +0600

    updaete-v4-beta

--- package/index.js
+++ package/index.js
@@ -1,7 +1,7 @@
 import { Router } from "./core/router.js";
 import { TezX } from "./core/server.js";
 export { Router, TezX };
-export let version = "3.0.2-beta";
+export let version = "3.0.4-beta";
 export default {
     Router,
     TezX,
